d404b0a2c7d724574bc2e857bab2908fecd36425
morningstar342 <100089006+morningstar342@users.noreply.github.com>
2023-01-19 23:20:05 +0200

    re

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import Header from '../components/header'
+import Footer from '../components/footer'
 
 const Home = () => {
     return (
-        <div>
-            
-        </div>
+        <>
+            <Header></Header>
+
+            <Footer/>
+        </>
     );
 }
 
