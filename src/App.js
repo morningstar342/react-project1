cd7438cca48f69848b52d63f117d40deab198458
morningstar342 <100089006+morningstar342@users.noreply.github.com>
2023-01-19 20:54:51 +0200

    update lesson 8

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 function App() {
   return (
     <>
+      {/*
+        Put all in body (MD)
+        ====================================================
+        ====================================================
+      */}
       <h1 className="title">I love React JS</h1>
       <p>i love react js , and python</p>
       <p>i love react js , and python</p>
--- src/index.js
+++ src/index.js
@@ -2,10 +2,36 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+// React Rounter Additions MS
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+// Import pages MS
+import Home from "./pages/home";
+import Page2 from "./pages/Page2";
+// import Page2 from "./pages/Page2";
+
+
+const router = createBrowserRouter([
+  {
+    // Add path + elements from pages imported MS
+    path: "/",
+    element: <Home/>,
+    errorElement: <h1>Sorry No Such Page or Any Imported Design like 'Error404'</h1>,
+  },
+  {
+    path: "/Page2",
+    element: <Page2/>,
+    errorElement: <h1>Sorry No Such Page or Any Imported Design like 'Error404'</h1>,
+  },
+]);
+
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+      <RouterProvider router={router} />
   </React.StrictMode>
 );
